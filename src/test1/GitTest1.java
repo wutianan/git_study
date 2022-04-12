464af18b51e09d1997b2f94c884921407d07c614
jeff <123@qq.com>
2022-04-12 10:35:59 +0800

    third idea

--- src/test1/GitTest1.java
+++ src/test1/GitTest1.java
@@ -8,5 +8,6 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("测试git");
         System.out.println("测试git2 ");
+        System.out.println("master test");
     }
 }
